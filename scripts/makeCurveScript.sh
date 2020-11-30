e027b84f8d162eab2db33abd8b51cdae82c23998
Daniel Zeiberg <zeiberg.d@northeastern.edu>
2020-11-30 10:16:10 -0500

    update local script

--- scripts/makeCurveScript.sh
+++ scripts/makeCurveScript.sh
@@ -15,7 +15,7 @@ do
 			echo "$F exists, skipping"
 		else
 			echo starting $loopNum - $jobNum;
-			matlab  -nodisplay -nosplash - nodesktop -r "curves = makeCurves('../data/syntheticParameters.mat',@(x,y)CurveConstructor(x,y), 'setNumber', "$jobNum", 'savePath','../data/curves_paramset_"$jobNum".mat','quiet',true);exit;" > logs/out_$jobNum.out &
+			matlab  -nodisplay -nosplash - nodesktop -r "curves = makeCurves('../data/syntheticParameters.mat',@(x,y)CurveConstructor(x,y), 'setNumberStart', "$jobNum", 'setNumberEnd', "$jobNum", 'savePath','../data/curves_paramset_"$jobNum".mat','quiet',true);exit;" > logs/out_$jobNum.out &
 			pids[${i}]=$!
 		fi
 	done
@@ -25,3 +25,26 @@ do
 	    wait $pid
 	done
 done
+
+
+# SetsPerJob=100;
+# TotalSets=100000;
+# JobsPerLoop=2;
+# let "TotalJobs=$TotalSets/$SetsPerJob"
+# let "NumLoops=$TotalJobs/$JobsPerLoop"
+# for ((loopNum=1; loopNum<=$NumLoops; loopNum++))
+# do
+# 	for ((n=1; n <=$JobsPerLoop; n++))
+# 	do
+# 		let "jobNum=$JobsPerLoop*($loopNum-1)+$n";
+# 	done
+# done
+
+
+# for ((i=1; i <=$TotalJobs; i++))
+# do 
+# 	let "Start=$SetsPerJob*($i-1)+1";
+# 	let "End=$i*$SetsPerJob";
+#     matlab -nodisplay -nosplash -nodesktop -r "curves=makeCurves('../data/syntheticParameters.mat', @(x,y)CurveConstructor(x,y),'setNumberStart',"$Start", 'setNumberEnd',"$End",'savePath','../data/curves_paramsets_"$Start"_"$End"','quiet',true);exit;"> logs/out_$Start_$End.out &
+#     pids[${i}]=$!
+# done
