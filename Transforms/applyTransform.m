4edbf573752250c46ac3c624bb33e52c04b9488a
Daniel Zeiberg <zeiberg.d@northeastern.edu>
2021-01-22 11:08:40 -0500

    add option for optimal transform

--- Transforms/applyTransform.m
+++ Transforms/applyTransform.m
@@ -5,7 +5,10 @@ function [preds,aucPU] = applyTransform(X,S,varargin)
     %   - transform : type of transform to apply : {rt: RegressionTree,
     %                                               nn: NeuralNetwork,
     %                                               svm: SVM,
-    %                                               none: no transform}
+    %                                               none: no transform,
+    %                                               optimal: apply all
+    %               transforms then choose one with the highest AUCPU}
+    
     % For all other optional arguments, see relevant file
     addpath("utilities");
     args= inputParser;
@@ -40,8 +43,54 @@ function [preds,aucPU] = applyTransform(X,S,varargin)
     elseif strcmp(args.transform, 'none')
         aucPU = get_auc_ultra(X,S);
         preds = X;
+    elseif strcmp(args.transform, 'optimal')
+        warning("This is going to take a while");
+        f = waitbar(0, 'finding optimal transform : nn1 : 0/6 complete');
+        pause(0.0000001);
+        results.nn1 = doNN(X,S,[1,1]);
+        f = waitbar(0, 'finding optimal transform : nn5 : 1/6 complete');
+        results.nn5 = doNN(X,S,[5,5]);
+        f = waitbar(0, 'finding optimal transform : nn25 : 2/6 complete');
+        results.nn25 = doNN(X,S,[25,25]);
+        f = waitbar(0, 'finding optimal transform : rt : 3/6 complete');
+        results.rt = doRT(X,S);
+        f = waitbar(0, 'finding optimal transform : svm1 : 4/6 complete');
+        results.svm1 = doSVM(X,S,1);
+        f = waitbar(0, 'finding optimal transform : svm2 : 5/6 complete');
+        results.svm2 = doSVM(X,S,2);
+        close(f);
+        tnames = fieldnames(results);
+        optTransform = "";
+        optAUCPU = .49;
+        for tnum = 1:numel(tnames)
+            if results.(tnames{tnum}).aucPU > optAUCPU
+                optTransform = tnames{tnum};
+                optAUCPU = results.(tnames{tnum}).aucPU;
+            end
+        end
+        preds = results.(optTransform).preds;
+        aucPU = results.(optTransform).aucPU;
     else
         error("Invalid transform, must be one of: {nn,rt,svm,none}");
     end
 end
 
+function [res] = doNN(X,S,hidden_layer_sizes)
+    mf = @()NeuralNetwork('hidden_layer_sizes',hidden_layer_sizes);
+    [res.preds,res.aucPU] = transform_bagging(X,S,mf,...
+           'val_frac',0.20,...
+           'num_bagged_models',100);
+end
+
+function [res] = doRT(X,S)
+    mf = @()RegressionTree();
+    [res.preds,res.aucPU] = transform_bagging(X,S,mf,...
+        'val_frac',0,...
+        'num_bagged_models',1000);
+end
+
+function [res] = doSVM(X,S,order)
+    [res.preds,res.aucPU] = transform_svm(X,S,'polynomialOrder',order,...
+            'kfoldvalue',10,...
+            'applyPlattCorrection',true);
+end
