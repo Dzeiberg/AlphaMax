6aa118e357c0a0dcffaccb87550e745a035c4069
Daniel Zeiberg <zeiberg.d@northeastern.edu>
2021-02-16 17:30:00 -0500

    modify testalphamax to explicitly use estimator net

--- tests/testalphamax.m
+++ tests/testalphamax.m
@@ -2,4 +2,4 @@ addpath("syntheticDataGeneration/");
 addpath("alphamax");
 ss = SyntheticSampler("data/syntheticParameters.mat");
 [p,u,alpha] = ss.getSample();
-[alphaHat,out] = runAlphaMax(u,p,'transform','none');
+[alphaHat,out] = runAlphaMax(u,p,'transform','none','useEstimatorNet',true);
