e797b87a1dbe8d5d78bebe6de08b4b6a600f7ccd
Daniel Zeiberg <zeiberg.d@northeastern.edu>
2021-02-16 17:02:04 -0500

    test file for running distcurve

---
+++ tests/testdistcurve.m
@@ -0,0 +1,5 @@
+addpath("syntheticDataGeneration/");
+addpath("distcurve");
+ss = SyntheticSampler("data/syntheticParameters.mat");
+[p,u,alpha] = ss.getSample();
+[alphaHat,curve, aucPU] = runDistCurve(u,p,'transform','none');
