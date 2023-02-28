52ff01e6270c62ead445c9b3450abd4a5d3ee535
Daniel Zeiberg <zeiberg.d@northeastern.edu>
2023-02-28 15:28:16 -0500

    remove old tests

--- tests/testdistcurve.m
+++
@@ -1,9 +0,0 @@
-addpath("syntheticDataGeneration/");
-addpath("distcurve");
-% ss = SyntheticSampler("data/syntheticParameters.mat");
-% [p,u,alpha] = ss.getSample();
-p = zeros(10,1);
-u = 1:100;
-u = u';
-[alphaHat,curve, aucPU] = runDistCurve(u,p,'transform','none');
-figure;plot(curve)
