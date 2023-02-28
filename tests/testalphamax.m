7c228331249cea5e18419dd56f5e7e490aba57d0
Daniel Zeiberg <zeiberg.d@northeastern.edu>
2023-02-28 15:28:05 -0500

    test alphamax

--- tests/testalphamax.m
+++ tests/testalphamax.m
@@ -1,5 +1,9 @@
 addpath("syntheticDataGeneration/");
 addpath("alphamax");
-ss = SyntheticSampler("data/syntheticParameters.mat");
-[p,u,alpha] = ss.getSample();
-[alphaHat,out] = runAlphaMax(u,p,'transform','none','useEstimatorNet',true);
+% ss = SyntheticSampler("data/syntheticParameters.mat");
+% [p,u,alpha] = ss.getSample();
+p = normrnd(3,2,1000,1);
+u = [normrnd(-3,2,7000,1);normrnd(3,2,3000,1)];
+path_to_estimator = '/home/dz/research/alphamax2/alphamax/estimators/alphamaxEstimator.mat';
+[alphaHat,out] = runAlphaMax(u,p,'transform','none','useEstimatorNet',true,...
+    'estimator',path_to_estimator);
